8ceb049a091e48b0f6f4b4714d0770f43a294315
Nagesh Singh <nst321888@gmail.com>
2022-12-15 14:14:57 +0530

    deleted previous code

--- myApp/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Provider from './contextAndProvider'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider><App /></Provider>);
--- myApp/src/record/NoRecordFound.jsx
+++
@@ -1,5 +0,0 @@
-import style from './NoRecordFound.module.css'
-
-export default function NoRecordFound(){
-    return(<h1 className={style.noRecordFoundH1}> <i className="fa-solid fa-file-excel"></i>&nbsp;No Record Found</h1>)
-}
